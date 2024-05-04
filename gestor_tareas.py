5167788f47e234aa09544325303a057a2357b8b1
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2024-05-04 21:20:30 +0200

    v.1.0.7 Se añade persistencia a la lista de tareas mediante la opción de guardar la lista en un archivo externo al programa. Además se codifica el archivo en base64 para darle un toque de privacidad. Tambien se añade una guia de uso.

--- gestor_tareas.py
+++ gestor_tareas.py
@@ -1,3 +1,8 @@
+# Importamos las librerias necesarias para guardar la lista en un 
+# archivo externo y para codificar el contenido del archivo en base64.
+import base64
+import json
+
 # Definimos la clase TaskManager
 class TaskManager:
 
@@ -45,6 +50,33 @@ class TaskManager:
             print("Error!!\n¡La tarea no existe en la lista de tareas!\n¡No se puede eliminar!")
 
 
+    # Creamos la funcion que nos creará en archivo y codificará en base64
+    def guardar_archivo(self, nombre_archivo):
+        # Aqui le decimos que abra el archivo en modo 'w' (write, escritura)
+        with open(nombre_archivo, 'w') as archivo:
+            # Ahora coge la lista de tareas (self.tareas) y la codifica en base64
+            tareas_codificadas = base64.b64encode(json.dumps(self.tareas).encode()).decode()
+            # Ahora escribe la lista de tareas codificada en el archivo.
+            archivo.write(tareas_codificadas)
+
+
+    # Creamos la funcion que nos cargará el archivo, lo decodificará deasde base64
+    # y lo cargará en la lista (self.tareas)
+    def cargar_desde_archivo(self, nombre_archivo):
+        # Aqui abre el archivo en modo lectura 'r'
+        try:
+            with open(nombre_archivo, 'r') as archivo:
+            # AHora lee el archivo codificado y lo carga en la variable tareas_codificadas
+                tareas_codificadas = archivo.read()
+            # Ahora decodifica desde base64 la variable tareas_codificadas, lo que resulta
+            # en la lista legible en formato diccionario y la carga en la lista de tareas (self.tareas)
+                self.tareas = json.loads(base64.b64decode(tareas_codificadas.encode()).decode())
+                print("Lista de tareas cargada!\nElige la opción 2 para ver las tareas.")
+        except FileNotFoundError:
+                print("Error!\nEl archivo no existe o no está en el mismo directorio que el programa!")
+        except (ValueError, IndexError) as petada1:
+                print(petada1)
+
 # Ahora creamos el objeto task_manager de la clase TaskManager()
 
 task_manager = TaskManager()
@@ -53,7 +85,7 @@ task_manager = TaskManager()
 # usando un ascii art
 print()
 print("░▀█▀▒▄▀▄░▄▀▀░█▄▀░░░█▄▒▄█░█░█▄░█░█▀▄▒██▀▒█▀▄")
-print("░▒█▒░█▀█▒▄██░█▒█▒░░█▒▀▒█░█░█▒▀█▒█▄▀░█▄▄░█▀▄ v.1.0.6")
+print("░▒█▒░█▀█▒▄██░█▒█▒░░█▒▀▒█░█░█▒▀█▒█▄▀░█▄▄░█▀▄ v.1.0.7")
 print("Created by: Antonio Gutiérrez")
 print("https://github.com/afgsanchez")
 print()
@@ -75,7 +107,10 @@ while True:
     print("2. Listar tareas.")
     print("3. Completar una tarea.")
     print("4. Eliminar una tarea.")
-    print("5. Salir de TASK MINDER.")
+    print("5. Guardar tareas en un archivo.")
+    print("6. Cargar archivo de tareas.")
+    print("7. Salir de TASK MINDER.")
+    print("8. Ayuda")
     print()
 
 # Creamos el input para que el usuario elija una de las opciones.
@@ -165,8 +200,34 @@ while True:
 # nos de el sistema, no los mensajes que le hayamos programado.
         except (ValueError, IndexError) as fallo:
             print(fallo)
-
+    
     elif seleccion == "5":
+        print("+-------------------------+")
+        print("| Has elegido la opción 5 |")
+        print("+-------------------------+")
+        print()
+        # Pedimos al usuario un nombre para el archivo que vamos a crear.
+        nombre_archivo = input("Dale un nombre al archivo (no es necesario extensión): ")
+        # Llamamos a la función guardar_archivo con el argumento del nombre de archivo
+        # para que nos lo cree. 
+        task_manager.guardar_archivo(nombre_archivo)
+        # Informamos al usuario que se ha creado el archivo.
+        print("Archivo guardado y codificado!\n")
+
+    elif seleccion == "6":
+        print("+-------------------------+")
+        print("| Has elegido la opción 6 |")
+        print("+-------------------------+")
+        print()
+        # Pedimos al usuario un nombre del archivo a cargar.
+        nombre_archivo = input("Escribe el nombre del archivo para cargar: ")
+        # Llamamos a la función cargar_desde_archivo con el argumento del nombre de archivo
+        # para que nos lo cargue en la lista de tareas.
+        task_manager.cargar_desde_archivo(nombre_archivo)
+        # Informamos al usuario que se ha cargado el archivo.
+                      
+
+    elif seleccion == "7":
         print("+---------------------------------+")
         print("| Has elegido SALIR del programa. |")
         print("+---------------------------------+")
@@ -174,11 +235,36 @@ while True:
         print("¡Muchas gracias por usar TASK MINDER!")
         print()
         print("░▀█▀▒▄▀▄░▄▀▀░█▄▀░░░█▄▒▄█░█░█▄░█░█▀▄▒██▀▒█▀▄")
-        print("░▒█▒░█▀█▒▄██░█▒█▒░░█▒▀▒█░█░█▒▀█▒█▄▀░█▄▄░█▀▄ v.1.0.6")
+        print("░▒█▒░█▀█▒▄██░█▒█▒░░█▒▀▒█░█░█▒▀█▒█▄▀░█▄▄░█▀▄ v.1.0.7")
         print("Created by: Antonio Gutiérrez")
         print("https://github.com/afgsanchez")
         print()
         break
+
+    elif seleccion == "8":
+        print("+-----------------------------------------+")
+        print("|             GUÍA DE USO                 |")
+        print("+-----------------------------------------+")
+        print("Bienvenido a Task Minder! Este programa te permite gestionar tus tareas de manera simple y eficiente.")
+        print("A continuación, te mostramos las opciones disponibles:")
+        print()
+        print("1. Agregar una tarea: Permite añadir una nueva tarea a la lista.")
+        print("2. Listar tareas: Muestra todas las tareas almacenadas, indicando su estado (completada o pendiente).")
+        print("3. Completar una tarea: Marca una tarea como completada.")
+        print("4. Eliminar una tarea: Elimina una tarea de la lista.")
+        print("5. Guardar tareas en un archivo: Guarda todas las tareas en un archivo codificado en base64.")
+        print("    - Al elegir esta opción, se te pedirá que escribas un nombre para el archivo.")
+        print("    - Asegúrate de escribir un nombre único y sin extensión.")
+        print("6. Cargar archivo de tareas: Carga las tareas desde un archivo previamente guardado.")
+        print("    - Al elegir esta opción, se te pedirá que escribas el nombre del archivo a cargar.")
+        print("    - Asegúrate de escribir el nombre exacto del archivo, sin extensión.")
+        print("7. Salir de Task Minder: Cierra el programa.")
+        print("8. Ayuda: Muestra esta guía de uso.")
+        print()
+        print("¡Esperamos que disfrutes usando Task Minder para organizar tus tareas!")
+        print("+-----------------------------------------+")
+        print()
+
 
     else:
         print("+--------------------------------------+")
